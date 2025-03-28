e30309a0f055bd94d83d2a0734ad17c0bb07d865
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-03-28 23:39:44 +0530

    solution update

--- Arrays/Move_all_negative_elements_to_end/Solution.java
+++ Arrays/Move_all_negative_elements_to_end/Solution.java
@@ -1,5 +1,34 @@
-package Move_all_negative_elements_to_end;
+/* Given an unsorted array arr[ ] having both negative and positive integers. The task is to place all negative elements at the end of the array without changing the order of positive elements and negative elements.
 
-public class Solution {
-    
+Input : arr[] = [1, -1, 3, 2, -7, -5, 11, 6 ]
+Output : [1, 3, 2, 11, 6, -1, -7, -5]
+
+ */
+
+ class Solution {
+    public void segregateElements(int[] arr) {
+        int n = arr.length;
+        int[] a = new int[n];
+        
+        int index = 0;
+        
+        for (int i = 0; i<n;i++){
+            if(arr[i] >= 0 ){
+                a[index] = arr[i];
+                index++;
+            }
+        }
+        
+        for (int i = 0; i<n;i++){
+            if(arr[i] < 0 ){
+                a[index] = arr[i];
+                index++;
+            }
+        }
+        
+        for (int i = 0; i<n;i++){
+            arr[i] = a[i];
+        }
+        
+    }
 }
--- Arrays/Sort_012/Solution.java
+++ Arrays/Sort_012/Solution.java
@@ -1,34 +1,61 @@
-/* Given an unsorted array arr[ ] having both negative and positive integers. The task is to place all negative elements at the end of the array without changing the order of positive elements and negative elements.
-
-Input : arr[] = [1, -1, 3, 2, -7, -5, 11, 6 ]
-Output : [1, 3, 2, 11, 6, -1, -7, -5]
-
+/* Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
  */
 
+// optimal solution with time complexity of O(n) 
+
 class Solution {
-    public void segregateElements(int[] arr) {
-        int n = arr.length;
-        int[] a = new int[n];
-        
-        int index = 0;
-        
-        for (int i = 0; i<n;i++){
-            if(arr[i] >= 0 ){
-                a[index] = arr[i];
-                index++;
-            }
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+       int count0 = 0 , count1 = 0 ,count2 = 0;
+       
+       for(int i=0;i < arr.length;i++){
+           if(arr[i] == 0) count0++;
+           else if (arr[i] == 1) count1++;
+           else count2++;
+       }
+       
+       int index = 0;
+       for (int i = 0; i<count0;i++ ){
+           arr[index++] = 0;
+       }
+       for(int i = 0 ; i<count1;i++){
+           arr[index++] = 1;
+       }
+       for(int i = 0 ; i<count2;i++){
+           arr[index++] = 2;
+       }
+       
+    }
+}
+
+
+// more optimized solution in single pass using dutch national flag algorithm
+
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void swap(int[] arr, int i, int j){
+        if (i != j) { // To avoid making both values zero if i == j
+            int temp = arr[i]; 
+            arr[i] = arr[j];    
+            arr[j] = temp;   
         }
-        
-        for (int i = 0; i<n;i++){
-            if(arr[i] < 0 ){
-                a[index] = arr[i];
-                index++;
-            }
-        }
-        
-        for (int i = 0; i<n;i++){
-            arr[i] = a[i];
-        }
-        
+    }
+    public void sort012(int[] arr) {
+       int low = 0, mid = 0 , high = arr.length-1;
+       
+       while(mid<=high){
+           if(arr[mid] == 0 ){
+               swap(arr, low, mid);
+               low++;
+               mid++;
+           }
+           else if (arr[mid] == 1){
+               mid++;
+           }
+           else{
+               swap(arr,mid,high);
+               high--;
+           }
+       }
     }
 }
