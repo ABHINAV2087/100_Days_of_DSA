a6a5f02bf7970676dc3c91583cd4cbb0befb3264
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-04-16 23:58:19 +0530

    update

--- Arrays/268. Missing Number/Solution.java
+++ Arrays/268. Missing Number/Solution.java
@@ -9,4 +9,14 @@ public class Solution {
         }
         return res;
     }
+
+public class Solution {
+    public int missingNumber(int[] nums) { //xor
+        int res = nums.length;
+        for(int i=0; i<nums.length; i++){
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
 }
