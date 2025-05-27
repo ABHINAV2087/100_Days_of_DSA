600714e665bee8e2b4246be86f35cd2bc8837c18
Abhinav Tirole <91676690+ABHINAV2087@users.noreply.github.com>
2025-05-27 16:26:18 +0530

    Update Solution.java

--- Arrays/268. Missing Number/Solution.java
+++ Arrays/268. Missing Number/Solution.java
@@ -1,22 +1,14 @@
 package 268. Missing Number;
 
-public class Solution {
-    public int missingNumber(int[] nums) { //xor
-        int res = nums.length;
-        for(int i=0; i<nums.length; i++){
-            res ^= i;
-            res ^= nums[i];
-        }
-        return res;
-    }
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        int actualSum = ((n*(n+1))/2);
 
-public class Solution {
-    public int missingNumber(int[] nums) { //xor
-        int res = nums.length;
-        for(int i=0; i<nums.length; i++){
-            res ^= i;
-            res ^= nums[i];
+        for(int i =0;i<n;i++){
+            sum += nums[i];
         }
-        return res;
+        return actualSum - sum;
     }
 }
