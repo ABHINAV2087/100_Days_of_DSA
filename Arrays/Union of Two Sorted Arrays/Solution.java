c546f34d0bdf6fda3b8cbf664b6359d8df56d16d
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-06-22 23:54:19 +0530

    update

--- Arrays/Union of Two Sorted Arrays/Solution.java
+++ Arrays/Union of Two Sorted Arrays/Solution.java
@@ -15,20 +15,3 @@ public class Solution {
         return ans;
     }
 }
-public class Solution {
-    // Function to return a list containing the union of the two arrays.
-    public static ArrayList<Integer> findUnion(int a[], int b[]) {
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int i = 0; i < a.length; i++) {
-            set.add(a[i]);
-        }
-        for(int j = 0; j < b.length; j++) {
-            set.add(b[j]);
-        }
-
-        ArrayList<Integer> ans = new ArrayList<>(set); 
-        Collections.sort(ans);
-        return ans;
-    }
-}
