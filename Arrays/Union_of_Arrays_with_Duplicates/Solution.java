d8674dd45a07f3b413f59cc91a9304754c408c61
Abhinav Tirole <abhinav.tirole2021@vitbhopal.ac.in>
2025-06-23 23:53:35 +0530

    update

--- Arrays/Union_of_Arrays_with_Duplicates/Solution.java
+++ Arrays/Union_of_Arrays_with_Duplicates/Solution.java
@@ -41,3 +41,29 @@ class Solution {
         return arr;
     }
 }
+// Intersection of two arrays
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i : nums2){
+            set1.add(i);
+        }
+
+        for( int i = 0 ;i < nums1.length; i++){
+            if(set1.contains(nums1[i])){
+                list.add(nums1[i]);
+                set1.remove(nums1[i]);
+            }
+        }
+
+        int[] arr = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            arr[i] = list.get(i);
+        }
+
+        return arr;
+    }
+}
